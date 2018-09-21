e29b946c41bfb23f45fbc8cdd40f5e81f97072e4
Christoph Taucher <taucher.ch@gmail.com>
2018-09-21 19:28:15 +0200

    refactor Charts with scale Axis

--- projects/dl-chart/src/lib/line-chart/line-chart.component.ts
+++ projects/dl-chart/src/lib/line-chart/line-chart.component.ts
@@ -1,13 +1,14 @@
 import { Component, OnInit, AfterViewInit, Input, ViewEncapsulation, OnChanges, SimpleChanges } from '@angular/core';  
 import { ChartItemService } from '../services/chart-item.service';
 import { Utils } from "../shared/utils";
-import { BaseChartComponent } from '../shared/base-chart.component';
+//import { BaseChartComponent } from '../shared/base-chart.component';
 import { ServiceItem } from '../models/serviceitem.model';
 import { Axis } from '../models/axis.model';
-import { ChartOrientation } from '../models/enums';
+//import { ChartOrientation } from '../models/enums';
 import { Line } from '../models/line.model';
 import { LinePoint } from '../models/linepoint.model';
 import { AxisPoint } from '../models/axispoint.model';
+import { ScaleBaseChartComponent } from '../shared/scale-base-chart.component';
 
 @Component({  
   selector: 'dl-line-chart',  
@@ -15,17 +16,12 @@ import { AxisPoint } from '../models/axispoint.model';
   styleUrls: ['./line-chart.component.scss'],
   encapsulation: ViewEncapsulation.Emulated
 })  
-export class LineChartComponent extends BaseChartComponent implements OnInit, AfterViewInit, OnChanges {
+export class LineChartComponent extends ScaleBaseChartComponent implements OnInit, AfterViewInit, OnChanges {
   
   viewBoxWidht: number = 450;
   viewBoxHeight: number = 450;
-  valueSteps: number = 6;
   currentScaleMaxValue: number = 10;
-  currentOrientation: ChartOrientation = ChartOrientation.Bottom;
-  currentScaleLabel: string = 'Values';
   currentValues: Line[] = [];
-  activeLeftScaleAxis: boolean = true;
-  activeRightScaleAxis: boolean = false;
   currentActiveLinePoint: LinePoint = null;
   currentActiveAxisPoint: AxisPoint = null;
   activeHideRaster: boolean = false;
@@ -41,16 +37,6 @@ export class LineChartComponent extends BaseChartComponent implements OnInit, Af
   }
 
   @Input()
-  set scaleLabel(val: string) {
-    this.currentScaleLabel = val;
-  }
-
-  @Input()
-  set steps(val: number) {
-    this.valueSteps = val;
-  }
-
-  @Input()
   set scaleMaxValue(val: number) {
     this.currentScaleMaxValue = val;
   }
@@ -63,201 +49,6 @@ export class LineChartComponent extends BaseChartComponent implements OnInit, Af
   @Input()
   set hideLines(val: boolean) {
     this.activeHideLines = val;
-  }
-
-  @Input()
-  set leftScaleAxis(val: boolean) {
-    this.activeLeftScaleAxis = val;
-  }
-
-  @Input()
-  set rightScaleAxis(val: boolean) {
-    this.activeRightScaleAxis = val;
-  }
-
-  @Input()
-  set orientation(val: ChartOrientation) {
-    this.currentOrientation = val;
-  }
-
-  get chartStyle() {
-    if (this.currentOrientation === ChartOrientation.Right) {
-      return {"transform" : "rotate(-90deg)"};
-    } else if (this.currentOrientation === ChartOrientation.Left) {
-      return {"transform" : "rotate(90deg)"};
-    } else if (this.currentOrientation === ChartOrientation.Top) {
-      return {"transform" : "rotate(180deg)"};
-    } else {
-      return {"transform" : "rotate(0deg)"};
-    }
-  }
-
-  get secondYAxisOrientation() {
-    if (this.currentOrientation === ChartOrientation.Top) {
-      return ChartOrientation.Bottom;
-    } else if (this.currentOrientation === ChartOrientation.Left) {
-      return ChartOrientation.Right;
-    } else if (this.currentOrientation === ChartOrientation.Right) {
-      return ChartOrientation.Left;
-    } else {
-      return ChartOrientation.Top;
-    }
-  }
-
-  get yAxisTextStyle() {
-    return this.onYAxisTextStyle(this.currentOrientation);
-  }
-
-  get yAxisTextStyleSecond() {
-    return this.onYAxisTextStyle(this.currentOrientation, true);
-  }
-
-  onYAxisTextStyle(orientation: ChartOrientation, reverseAnchor: boolean = false) {
-    if (orientation === ChartOrientation.Right) {
-      return {"transform" : "rotate(90deg)", "text-anchor" : "middle"};
-    } else if (orientation === ChartOrientation.Left) {
-      return {"transform" : "rotate(-90deg)", "text-anchor" : "middle"};
-    } else if (orientation === ChartOrientation.Top) {
-      return {"transform" : "rotate(180deg)", "text-anchor" : (reverseAnchor ? "end" : "start") };
-    } else {
-      return {"transform" : "rotate(0deg)", "text-anchor" : (reverseAnchor ? "start" : "end") };
-    }
-  }
-
-  get yAxisTextXPositionOffset() {
-    return this.onYAxisTextXPositionOffset(this.currentOrientation);
-  }
-
-  get yAxisTextXPositionOffsetSecond() {
-    return this.onYAxisTextXPositionOffset(this.secondYAxisOrientation);
-  }
-
-  onYAxisTextXPositionOffset(orientation: ChartOrientation) {
-    if (orientation === ChartOrientation.Right) {
-      return 0;
-    } else if (orientation === ChartOrientation.Left) {
-      return 0;
-    } else if (orientation === ChartOrientation.Top) {
-      return 9;
-    } else {
-      return -9;
-    }
-  }
-
-  get yAxisTextYPositionOffset() {
-    return this.onYAxisTextYPositionOffset(this.currentOrientation);
-  }
-
-  get yAxisTextYPositionOffsetSecond() {
-    return this.onYAxisTextYPositionOffset(this.secondYAxisOrientation);
-  }
-
-  onYAxisTextYPositionOffset(orientation: ChartOrientation) {
-    if (orientation === ChartOrientation.Right) {
-      return 12;
-    } else if (orientation === ChartOrientation.Left) {
-      return -12;
-    } else if (orientation === ChartOrientation.Top) {
-      return 0;
-    } else {
-      return 0;
-    }
-  }
-
-  get yAxisLabelTextStyle() {
-    return this.onYAxisLabelTextStyle(this.currentOrientation);
-  }
-
-  get yAxisLabelTextStyleSecond() {
-    return this.onYAxisLabelTextStyle(this.currentOrientation);
-  }
-
-  onYAxisLabelTextStyle(orientation: ChartOrientation) {
-    if (orientation === ChartOrientation.Right) {
-      return {"transform" : "rotate(90deg)", "text-anchor" : "start"};
-    } else if (orientation === ChartOrientation.Left) {
-      return {"transform" : "rotate(-90deg)", "text-anchor" : "end"};
-    } else if (orientation === ChartOrientation.Top) {
-      return {"transform" : "rotate(90deg)", "text-anchor" : "start"};
-    } else {
-      return {"transform" : "rotate(-90deg)", "text-anchor" : "end"};
-    }
-  }
-
-  get yAxisLabelTextYPositionOffset() {
-    return this.onYAxisLabelTextYPositionOffset(this.currentOrientation);
-  }
-
-  get yAxisLabelTextYPositionOffsetSecond() {
-    if (this.currentOrientation === ChartOrientation.Right) {
-      return -445;
-    } else if (this.currentOrientation === ChartOrientation.Left) {
-      return 438;
-    } else if (this.currentOrientation === ChartOrientation.Top) {
-      return -445;
-    } else {
-      return 438;
-    }
-  }
-
-  onYAxisLabelTextYPositionOffset(orientation: ChartOrientation) {
-    if (orientation === ChartOrientation.Right) {
-      return -12;
-    } else if (orientation === ChartOrientation.Left) {
-      return 6;
-    } else if (orientation === ChartOrientation.Top) {
-      return -12;
-    } else {
-      return 6;
-    }
-  }
-
-  get xAxisTextStyle() {
-    return this.onXAxisTextStyle(this.currentOrientation);
-  }
-
-  onXAxisTextStyle(orientation: ChartOrientation) {
-    if (orientation === ChartOrientation.Right) {
-      return {"transform" : "rotate(90deg)", "text-anchor" : "start"};
-    } else if (orientation === ChartOrientation.Left) {
-      return {"transform" : "rotate(-90deg)", "text-anchor" : "end"};
-    } else if (orientation === ChartOrientation.Top) {
-      return {"transform" : "rotate(180deg)", "text-anchor" : "middle"};
-    } else {
-      return {"transform" : "rotate(0deg)", "text-anchor" : "middle"};
-    }
-  }
-
-  get xAxisTextXPositionOffset() {
-    return this.onXAxisTextXPositionOffset(this.currentOrientation);
-  }
-
-  onXAxisTextXPositionOffset(orientation: ChartOrientation) {
-    if (orientation === ChartOrientation.Right) {
-      return 9;
-    } else if (orientation === ChartOrientation.Left) {
-      return -9;
-    } else if (orientation === ChartOrientation.Top) {
-      return 0;
-    } else {
-      return 0;
-    }
-  }
-
-  get xAxisTextYPositionOffset() {
-    return this.onXAxisTextYPositionOffset(this.currentOrientation);
-  }
-
-  onXAxisTextYPositionOffset(orientation: ChartOrientation) {
-    if (orientation === ChartOrientation.Right) {
-      return -3.5;
-    } else if (orientation === ChartOrientation.Left) {
-      return -3.5;
-    } else if (orientation === ChartOrientation.Top) {
-      return -15;
-    } else {
-      return 9;
-    }
   }
 
   constructor(chartItemService: ChartItemService) {
