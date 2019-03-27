23d839350b435cf7daa83c8e24bab7d0a1f39c01
Christoph Taucher <taucher.ch@gmail.com>
2019-03-27 18:39:50 +0100

    move Y Axis to shared folder

--- projects/dl-chart/src/lib/line-chart/line-chart.module.ts
+++ projects/dl-chart/src/lib/line-chart/line-chart.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { LineChartComponent } from './line-chart.component';
 import { CommonModule } from '@angular/common';
 import { ChartItemService } from '../services/chart-item.service';
-import { YAxisComponent } from './yaxis.component';
+import { YAxisComponent } from '../shared/yaxis.component';
 
 
 @NgModule({
