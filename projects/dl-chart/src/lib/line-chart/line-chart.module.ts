6005fc5001810a634c26c9df309a176df814e7fc
Christoph Taucher <taucher.ch@gmail.com>
2019-03-27 17:24:12 +0100

    split Y Axis from line chart

--- projects/dl-chart/src/lib/line-chart/line-chart.module.ts
+++ projects/dl-chart/src/lib/line-chart/line-chart.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { LineChartComponent } from './line-chart.component';
 import { CommonModule } from '@angular/common';
 import { ChartItemService } from '../services/chart-item.service';
+import { YAxisTextComponent } from './yaxis-text.component';
+import { YAxisComponent } from './yaxis.component';
 
 
 @NgModule({
@@ -9,6 +11,8 @@ import { ChartItemService } from '../services/chart-item.service';
     CommonModule
   ],
   declarations: [
+    YAxisComponent,
+    YAxisTextComponent,
     LineChartComponent
   ],
   providers: [
---
+++ projects/dl-chart/src/lib/line-chart/yaxis.component.ts
@@ -0,0 +1,66 @@
+import { Component, Input } from '@angular/core';
+import { Axis } from '../models/axis.model';
+
+@Component({
+  selector: '[yaxis]',
+  templateUrl: './yaxis.component.html',
+  styleUrls: ['./yaxis.component.scss'],
+})
+export class YAxisComponent {
+
+  currentSVGX: string = '';
+  currentSVGY: string = '';
+  currentScaleLabel: string = '';
+  currentActiveScaleAxis: boolean = true;
+  currentYAxis: Axis[] = [];
+  currentIsLeft: boolean = true;
+
+  innerLineX2: string = '';
+  innerLineX1: string = '';
+  innerTextX: string = '';
+  innerTextStyle: string = '';
+  lineX1: string = '';
+  lineX2: string = '';
+
+  @Input()
+  set yAxis(val: Axis[]) {
+    this.currentYAxis = val;
+  }
+
+  @Input()
+  set activeScaleAxis(val: boolean) {
+    this.currentActiveScaleAxis = val;
+  }
+
+  @Input()
+  set svgX(val: string) {
+    this.currentSVGX = val;
+  }
+
+  @Input()
+  set svgY(val: string) {
+    this.currentSVGY = val;
+  }
+
+  @Input()
+  set isLeft(val: boolean) {
+    this.currentIsLeft = val;
+    if (this.currentIsLeft) {
+      this.innerLineX1 = 'calc(100% - 6px)';
+      this.innerLineX2 = '100%';
+      this.innerTextX = '75%';
+      this.innerTextStyle = 'end';
+      //this.innerTextStyle = 'text-anchor:end';
+      this.lineX1 = '100%';
+      this.lineX2 = '100%';
+    } else {
+      this.innerLineX1 = '0';
+      this.innerLineX2 = '6';
+      this.innerTextX = '8';
+      this.innerTextStyle = 'start';
+      //this.innerTextStyle = 'text-anchor:start';
+      this.lineX1 = '0';
+      this.lineX2 = '0';
+    }
+  }
+}
